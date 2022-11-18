5c2e90885e26d205b6d1fdd31578c8169db711a1
Anu <111705995+Madagoni123@users.noreply.github.com>
2022-11-18 12:17:17 -0600

    final changes

--- app.js
+++ app.js
@@ -46,13 +46,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({ 
-  secret: 'keyboard cat', 
-  resave: false, 
-  saveUninitialized: false 
-})); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -60,6 +54,15 @@ app.use('/users', usersRouter);
 app.use('/organisation',organisationRouter );
 app.use('/gridbuild',gridbuildRouter);
 app.use('/selector', selectorRouter);
+
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session());
+
 app.use('/resource', resourceRouter);
 
 
