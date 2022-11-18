8f933e2f8bb5ddc1d941cf85f4913d8dc2a7518d
Anu <111705995+Madagoni123@users.noreply.github.com>
2022-11-18 11:34:07 -0600

    final changes

--- app.js
+++ app.js
@@ -116,14 +116,13 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser()); 
 passport.deserializeUser(Account.deserializeUser()); 
  
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
