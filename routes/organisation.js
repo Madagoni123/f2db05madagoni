8285dc399e53ffc885ea4392bd4a3b9464456c05
Anu <111705995+Madagoni123@users.noreply.github.com>
2022-11-30 17:42:35 -0600

    added files

--- routes/organisation.js
+++ routes/organisation.js
@@ -1,21 +1,31 @@
 var express = require('express'); 
 const organisation_controlers= require('../controllers/organisation'); 
 var router = express.Router(); 
+// A little function to check if we have an authorized user and continue on 
+
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET costumes */ 
-router.get('/', organisation_controlers.organisation_view_all_Page ); 
+router.get('/',secured , organisation_controlers.organisation_view_all_Page ); 
 module.exports = router; 
 
 /* GET detail organisation page */ 
-router.get('/detail', organisation_controlers.organisation_view_one_Page); 
+router.get('/detail', secured ,organisation_controlers.organisation_view_one_Page); 
 
 /* GET create organisation page */ 
-router.get('/create', organisation_controlers.organisation_create_Page); 
+router.get('/create',secured , organisation_controlers.organisation_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', organisation_controlers.organisation_update_Page); 
+router.get('/update',secured , organisation_controlers.organisation_update_Page); 
 
 /* GET delete organisation page */ 
-router.get('/delete', organisation_controlers.organisation_delete_Page); 
+router.get('/delete',secured , organisation_controlers.organisation_delete_Page); 
  
  
